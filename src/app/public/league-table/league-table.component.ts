86d895cbc2579eab38d7e0930f52abe09e76f7d0
Suraj Shrestha <suraz.zarus@gmail.com>
2018-03-23 01:03:39 +0100

    table title added

--- src/app/public/league-table/league-table.component.ts
+++ src/app/public/league-table/league-table.component.ts
@@ -8,14 +8,16 @@ import {FootballService} from '../../core/services/football.service';
 })
 export class LeagueTableComponent implements OnInit {
   standings;
+  leagueYear: string;
 
   constructor(private footballService: FootballService) { }
 
   ngOnInit() {
     this.footballService.getTable()
       .subscribe((res: any) => {
-        console.log(res.standing)
+        console.log(res)
         this.standings = res.standing;
+        this.leagueYear = res.leagueCaption;
       });
   }
 
